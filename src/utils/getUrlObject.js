33a957f75438c2fb9167a96fbf296a9e47be2f13
Vladimir <git_109@list.ru>
2022-07-30 21:26:38 +0300

    create a component-helper to generate url

---
+++ src/utils/getUrlObject.js
@@ -0,0 +1,11 @@
+export const getUrlObject = (url = '', params = {}) => {
+  const urlObj = new URL(url)
+
+  Object.keys(params).forEach(param => {
+    const value = params[param].toISOString ? params[param].toISOString() : params[param]
+
+    return urlObj.searchParams.set(param, value)
+  })
+
+  return urlObj
+}
