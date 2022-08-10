0f74afbcfc085352b99d8cdd8f20c96f5ed4a2bc
Vladimir <git_109@list.ru>
2022-08-10 20:59:42 +0300

    create the Product list page

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,36 +1,123 @@
-import ProductForm from '../../../components/product-form'
+import SortableTable from '../../../components/sortable-table/index.js'
+import RangeSlider from '../../../components/range-slider/index.js'
+import header from './products-list-header'
 
-export default class Page {
+export default class Products {
   element
   subElements = {}
   components = {}
 
+  updateTableComponent(params) {
+    this.components.sortableTable.update(params)
+  }
+
+  async initComponent() {
+    const url = `${process.env.BACKEND_URL}api/rest/products?_embed=subcategory.category`
+
+    const sortableTable = new SortableTable(header, {
+      url,
+      isSortLocally: false,
+      start: 1,
+      end: 30,
+      step: 30
+    })
+
+    const rangeSlider = new RangeSlider({ max: 4000, prefix: '$' })
+
+    this.components.sortableTable = sortableTable
+    this.components.rangeSlider = rangeSlider
+  }
+
+  get template() {
+    return `
+    <div class="products-list full-height flex-column">
+      <div class="content__top-panel">
+        <h2 class="page-title">Products</h2>
+        <a href="/products/add" class="button-primary">Add a product</a>
+      </div>
+      <div class="content-box content-box_small">
+
+        <form class="form-inline">
+
+          <div class="form-group">
+            <label class="form-label">Sort by:</label>
+            <input data-elem="filterName" type="text" class="form-control" placeholder="Product's name" />
+          </div>
+          
+          <div data-elem="rangeSlider" class="form-group">
+            <label class="form-label">Price:</label>
+            <!-- range-slider component -->
+          </div>
+
+          <div class="form-group">
+            <label class="form-label">Status:</label>
+            <select data-elem="filterStatus" class="form-control">
+              <option value selected>Anything</option>
+              <option value="1">Active</option>
+              <option value="0">Inactive</option>
+            </select>
+          </div>
+        
+        </form>
+      </div>
+
+      <div data-elem="sortableTable" class="product-list__container">
+        <!-- sortable-table component -->
+      </div>
+
+    </div>`
+  }
+
   async render() {
-    const element = document.createElement('div')
+    const wrapper = document.createElement('div')
 
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`
+    wrapper.innerHTML = this.template
 
-    this.element = element.firstElementChild
+    this.element = wrapper.firstElementChild
+    this.subElements = this.getSubElements(this.element)
 
-    this.initComponents()
-    await this.renderComponents()
+    await this.initComponent()
+
+    this.renderComponents()
+
+    this.initEventListeners()
 
     return this.element
   }
 
-  initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj'
+  getSubElements($element) {
+    const elements = $element.querySelectorAll('[data-elem]')
 
-    this.components.productFrom = new ProductForm(productId)
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.elem] = subElement
+
+      return accum
+    }, {})
   }
 
-  async renderComponents() {
-    const element = await this.components.productFrom.render()
+  renderComponents() {
+    Object.keys(this.components).forEach(component => {
+      const root = this.subElements[component]
+      const { element } = this.components[component]
+      root.append(element)
+    })
+  }
 
-    this.element.append(element)
+  initEventListeners() {
+    const { rangeSlider, filterName, filterStatus } = this.subElements
+
+    rangeSlider.addEventListener('slider-change', event => {
+      const { from, to } = event.detail
+      this.updateTableComponent({ price_gte: from, price_lte: to })
+    })
+
+    filterName.addEventListener('input', ({ target }) => {
+      this.updateTableComponent({ title_like: target.value })
+    })
+
+    filterStatus.addEventListener('input', ({ target }) => {
+      this.updateTableComponent({ status: target.value })
+    })
   }
 
   destroy() {
---
+++ src/pages/products/list/products-list-header.js
@@ -0,0 +1,51 @@
+const header = [
+  {
+    id: 'images',
+    title: 'Image',
+    sortable: false,
+    template: data => {
+      return `
+          <td class="sortable-table__cell">
+            <img class="sortable-table-image" alt="Image" src="${data[0].url}">
+          </td>
+        `
+    }
+  },
+  {
+    id: 'title',
+    title: 'Name',
+    sortable: true,
+    sortType: 'string'
+  },
+  {
+    id: 'subcategory.title',
+    title: 'Category',
+    sortable: false,
+    sortType: 'number'
+  },
+  {
+    id: 'quantity',
+    title: 'Quantity',
+    sortable: true,
+    sortType: 'number'
+  },
+  {
+    id: 'price',
+    title: 'Price',
+    sortable: true,
+    sortType: 'number'
+  },
+  {
+    id: 'status',
+    title: 'Status',
+    sortable: true,
+    sortType: 'number',
+    template: data => {
+      return `<td class="sortable-table__cell">
+          ${data > 0 ? 'Active' : 'Inactive'}
+        </td>`
+    }
+  }
+]
+
+export default header
