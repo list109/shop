93f9dcc3ff47e7568073c66cfb6c3a74f46de596
Vladimir <git_109@list.ru>
2022-08-17 13:45:26 +0300

    update a page

--- src/pages/products/edit/index.js
+++ src/pages/products/edit/index.js
@@ -1,4 +1,6 @@
-export default class Page {
+import ProductForm from '../../../components/product-form/index.js'
+
+export default class Edit {
   element
   subElements = {}
   components = {}
@@ -6,13 +8,68 @@ export default class Page {
   async render() {
     const element = document.createElement('div')
 
-    element.innerHTML = `
-      <div>
-        <h1>Edit page</h1>
-      </div>`
+    element.innerHTML = this.template
 
     this.element = element.firstElementChild
 
+    this.subElements = this.getSubElements(this.element)
+
+    this.initComponents()
+
+    await this.renderComponents()
+
     return this.element
+  }
+
+  get template() {
+    return `
+      <div class="products-edit">
+        <div class="content__top-panel">
+          <h1 class="page-title">
+            <a href="/products" class="link">Products</a> / Edit
+          </h1>
+        </div>
+        <div class="content-box full-height">
+          <div class="product-form" data-elem="productForm"></div>
+        </div>
+      </div>
+    `
+  }
+
+  initComponents() {
+    const productId = location.pathname.split('/').slice(-1)[0]
+    const productForm = new ProductForm(productId)
+
+    this.components.productForm = productForm
+  }
+
+  async renderComponents() {
+    Object.keys(this.components)
+      .filter(component => component !== 'productForm')
+      .forEach(component => {
+        const root = this.subElements[component]
+        const { element } = this.components[component]
+
+        root.append(element)
+      })
+
+    const element = await this.components.productForm.render()
+    this.subElements.productForm.append(element)
+  }
+
+  getSubElements($element) {
+    const elements = $element.querySelectorAll('[data-elem]')
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.elem] = subElement
+
+      return accum
+    }, {})
+  }
+
+  destroy() {
+    for (const component of Object.values(this.components)) {
+      component.destroy()
+    }
   }
 }
