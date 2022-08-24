617acaeece4bc254f491da898866a704273f8234
Vladimir <git_109@list.ru>
2022-08-24 16:00:16 +0300

    update the mock globals

--- src/mocks/global-properties.js
+++ src/mocks/global-properties.js
@@ -4,6 +4,24 @@ Object.defineProperty(window.HTMLHtmlElement.prototype, 'clientHeight', {
   }
 })
 
+Object.defineProperty(window.HTMLHtmlElement.prototype, 'offsetHeight', {
+  get: function () {
+    return this._mockOffsetHeight || 0
+  }
+})
+
+Object.defineProperty(window.HTMLElement.prototype, 'offsetHeight', {
+  get: function () {
+    return this._mockOffsetHeight || 0
+  }
+})
+
+Object.defineProperty(window.HTMLHtmlElement.prototype, 'offsetWidth', {
+  get: function () {
+    return this._mockOffsetWidth || 0
+  }
+})
+
 Object.defineProperty(window.HTMLElement.prototype, 'getBoundingClientRect', {
   value: function getBoundingClientRect() {
     return this._mockClientRect || Element.prototype.getBoundingClientRect.call(this)
@@ -16,3 +34,5 @@ Object.defineProperty(window.HTMLElement.prototype, 'trim', {
     return this
   }
 })
+
+Event.prototype.button = 1
