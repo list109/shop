d79f40ccea353b5cb9eda0177b0141276d39e189
Vladimir <git_109@list.ru>
2023-02-04 14:34:45 +0300

    create a new sortable-table pattern

---
+++ src/components/sortable-table/patterns/sellers.js
@@ -0,0 +1,64 @@
+const header = [
+  {
+    id: 'images',
+    title: 'Фото',
+    sortable: false,
+    template: data => {
+      return `
+          <td class="sortable-table__cell">
+            <img class="sortable-table-image" alt="Image" src="${data[0]?.url}">
+          </td>
+        `
+    }
+  },
+  {
+    id: 'title',
+    title: 'Имя',
+    sortable: true,
+    sortType: 'string'
+  },
+  {
+    id: 'subcategory',
+    title: 'Категория',
+    sortable: false,
+    sortType: 'number',
+    template: data => {
+      const { title: subTitle } = data
+      const { title: catTitle } = data.category
+
+      const tooltip = `
+      <div class=&quot;sortable-table-tooltip&quot;>
+        <span class=&quot;sortable-table-tooltip__category&quot;>${catTitle}</span> /
+        <b class=&quot;sortable-table-tooltip__subcategory&quot;>${subTitle}</b>
+      </div>`
+
+      return `<td class="sortable-table__cell">
+      <span data-tooltip="${tooltip}">${subTitle}</span></td>`
+    }
+  },
+  {
+    id: 'quantity',
+    title: 'Количество',
+    sortable: true,
+    sortType: 'number'
+  },
+  {
+    id: 'price',
+    title: 'Цена',
+    sortable: true,
+    sortType: 'number',
+    template: data => {
+      return `<td class="sortable-table__cell">
+          $${data}
+        </td>`
+    }
+  },
+  {
+    id: 'sales',
+    title: 'Продажи',
+    sortable: true,
+    sortType: 'number'
+  }
+]
+
+export default header
